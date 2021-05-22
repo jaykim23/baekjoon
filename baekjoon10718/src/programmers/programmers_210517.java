387835ff954f99a81dd75ead2aec6bed2d5adf89
jaykim23 <jaykim23@naver.com>
2021-05-22 12:20:40 +0900

    programmers_210517

--- baekjoon10718/src/programmers/programmers_210517.java
+++ baekjoon10718/src/programmers/programmers_210517.java
@@ -4,7 +4,12 @@ public class programmers_210517 {
 
 	class Solution {
 		public boolean solution(String s) {
-	        char temp;	
+			
+//			https://programmers.co.kr/learn/courses/30/lessons/12918
+//			문자열 s의 길이가 4 혹은 6이고, 숫자로만 구성돼있는지 확인해주는 함수, solution을 완성하세요. 
+//			예를 들어 s가 "a234"이면 False를 리턴하고 "1234"라면 True를 리턴하면 됩니다.
+			
+			char temp;	
 	        boolean answer = true;
 	        //길이 비교
 	        if(s.length() == 4 || s.length() == 6){
