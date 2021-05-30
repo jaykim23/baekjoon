a4820410eb14fb30c1a2d69770f82abb9ba9497b
jaykim23 <jaykim23@naver.com>
2021-05-30 14:06:53 +0900

    programmers_210528

--- domestic_code_solutioon/src/programmers/Programmers_210528.java
+++ domestic_code_solutioon/src/programmers/Programmers_210528.java
@@ -9,20 +9,26 @@ public class Programmers_210528 {
 	//예를들어 n이 118372면 873211을 리턴하면 됩니다.
 	class Solution {
 	    public long solution(long n) {
+	    	//주어진 long 타입의 수 n을 String으로 변환한다.
 	        String num = String.valueOf(n);
+	        //문자열 답안을 선언한다.
 			String strAnswer = "";
+			//문자열 배열을 선언한다. 길이는 num의 문자열 배열의 길이를 집어넣는다.
 			char[] arr = new char[num.length()];
+			//num 문자열의 글자를 arr에 넣는다.
 			for(int i=0;i<arr.length;i++) {
 				arr[i] = num.charAt(i);
 			}
-			
+			//arr 배열을 오름차순 정렬한다.
 			Arrays.sort(arr);
+			//arr 배열을 문자열 strAnswer에 넣는다.
 			for(int i=arr.length-1;i>=0;i--) {
 				strAnswer += arr[i];
 			}
-			
+			//다시 long으로 형변환을 한다.
 			long answer = Long.parseLong(strAnswer);
-	        return answer;
+	        //리턴한다.
+			return answer;
 	    }
 	}
 }
