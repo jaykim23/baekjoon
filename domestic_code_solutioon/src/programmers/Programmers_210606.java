271d291975f6b5dd50c7e82f2c237c8e1d8b0ceb
jaykim23 <jaykim23@naver.com>
2021-06-06 02:44:14 +0900

    programmers_210606

---
+++ domestic_code_solutioon/src/programmers/Programmers_210606.java
@@ -0,0 +1,15 @@
+package programmers;
+
+public class Programmers_210606 {
+	// 출처 : https://programmers.co.kr/learn/courses/30/lessons/12937
+	// 정수 num이 짝수일 경우 "Even"을 반환하고 홀수인 경우 "Odd"를 반환하는 함수, solution을 완성해주세요.
+	public String solution(int num) {
+        String answer = "";
+        if(num % 2 ==0){
+            answer = "Even";
+        }else{
+            answer = "Odd";
+        }
+        return answer;
+    }
+}
