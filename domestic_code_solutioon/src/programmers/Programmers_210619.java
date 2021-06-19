05d4af931ca968ca959245a16eb74fed8cc88c94
jaykim23 <jaykim23@naver.com>
2021-06-19 13:37:11 +0900

    programmers_2106019

---
+++ domestic_code_solutioon/src/programmers/Programmers_210619.java
@@ -0,0 +1,21 @@
+package programmers;
+
+public class Programmers_210619 {
+	// 출처 : https://programmers.co.kr/learn/courses/30/lessons/12925
+	//	문자열 s를 숫자로 변환한 결과를 반환하는 함수, solution을 완성하세요.
+	//
+	//	제한 조건
+	//	s의 길이는 1 이상 5이하입니다.
+	//	s의 맨앞에는 부호(+, -)가 올 수 있습니다.
+	//	s는 부호와 숫자로만 이루어져있습니다.
+	//	s는 "0"으로 시작하지 않습니다.
+	//	입출력 예
+	//	예를들어 str이 "1234"이면 1234를 반환하고, "-1234"이면 -1234를 반환하면 됩니다.
+	public static void main(String[] args) {
+	    String s = "-124";    
+		int answer = 0;
+	    answer = Integer.parseInt(s);
+	}
+
+
+}
