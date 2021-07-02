3943cccbfb14cd9e1afd832f024cbb6830598e04
jaykim23 <jaykim23@naver.com>
2021-07-02 12:25:16 +0900

    programmers_210701

--- domestic_code_solutioon/src/programmers/Programmers_210701.java
+++ domestic_code_solutioon/src/programmers/Programmers_210701.java
@@ -7,13 +7,17 @@ public class Programmers_210701 {
 	//	n이 양의 정수 x의 제곱이 아니라면 -1을 리턴하는 함수를 완성하세요.
 	 public long solution(long n) {
 	        long answer = 0;
+	        //주어진 수 n까지 i를 제곱한다. 같은 경우 제곱근
 	        for (long i = 1; i <= n; i++) { 
+	        	//같을 때 answer에 i+1을 넣는다.
 	            if (i * i == n) { 
 	                answer = (i + 1) * (i + 1); 
 	                break; 
 	            } 
+	            //아닌경우 -1리턴
 	            else answer = -1; 
 	        }
+	        //리턴한다.
 	        return answer;
 	    }
 }
